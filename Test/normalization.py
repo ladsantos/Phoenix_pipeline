feac078c60e3fa0ac23b8f3275e3120a1eb02b12
Jayshil <jayshilpatel147@gmail.com>
2021-02-14 02:27:08 +0530

    Test files

---
+++ Test/normalization.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import SpanSelector
+import os
+from pathlib import Path
+from astropy.nddata import CCDData
+import ccdproc as ccdp
+from astropy.stats import mad_std
+import utils as utl
+import astropy.units as u
+import shutil
+
+def normal_spectrum(flux_file, out_path, degree=3):
+    """
+    Parameters:
+    -----------
+    flux_file : .dat file
+        flux file generated from flux_extraction.py file
+    out_path : str
+        Path of the output data file
+    degree : int
+        degree of the fitted polynomial
+        default is 3
+    -----------
+    returns
+    -----------
+    normal_flux : .dat file
+        data file containing pixel, normalized flux and error in normalized flux.
+    """
+    pix, fl, fle = np.loadtxt(flux_file, usecols=(0,1,2), unpack=True)
+    sigmas = np.copy(fle)
+    # ---------------------------------------
+    # To select the region of spectral lines
+    # ---------------------------------------
+    selection = []
+    fig = plt.figure(figsize=(16, 12))
+    ax = fig.add_subplot(211)
+
+    ax.errorbar(pix, fl, yerr=fle)
+    ax.grid()
+    #ax.set_ylim(-2, 2)
+    ax.set_title('Press left mouse button and drag to select the region of the spectrum you want to mask.')
+
+    ax2 = fig.add_subplot(212)
+    line2, = ax2.plot(pix, fl, '-')
+    ax2.grid()
+
+    def onselect(xmin, xmax):
+        indmin, indmax = np.searchsorted(pix, (xmin, xmax))
+        indmax = min(len(pix) - 1, indmax)
+
+        thisx = pix[indmin:indmax]
+        thisy = fl[indmin:indmax]
+        line2.set_data(thisx, thisy)
+        ax2.set_xlim(thisx[0], thisx[-1])
+        ax2.set_ylim(thisy.min(), thisy.max())
+        ax2.grid()
+        fig.canvas.draw_idle()
+        ab = [indmin, indmax]
+        selection.append(ab)
+
+    # set useblit True on gtkagg for enhanced performance
+    span = SpanSelector(ax, onselect, 'horizontal', useblit=True, 
+                         rectprops=dict(alpha=0.5, facecolor='red'))
+
+    plt.show()
+    #print(selection)
+    #print(selection[0][1])
+
+    for i in range(len(selection)):
+        sigmas[int(selection[i][0]):int(selection[i][1])] = 0
+    
+    bb = np.polyfit(pix, fl, w=sigmas, deg=degree)
+    # Remeber that polyfit gives you an array of the
+    # coefficient of the polynomial, starting with the highest degree.
+    # while the function in the utilities uses the array
+    # starting with a coefficients of lowest degree.
+    #print(bb)
+    coefs = np.flip(bb)
+    polynom = utl.arbi_poly(pix, *coefs)
+    normal_flux = fl/polynom
+    normal_flux_err = fle/polynom
+    flux_file1 = 'normal_' + flux_file
+    f11 = open(out_path + '/' + flux_file1, 'w')
+    for i in range(len(pix)):
+        f11.write(str(pix[i]) + '\t' + str(normal_flux[i]) + '\t' + str(normal_flux_err[i]) + '\n')
+    f11.close()
---
+++ Test/utils.py
@@ -0,0 +1,133 @@
+import numpy as np
+from astropy import visualization as aviz
+from astropy.nddata.utils import block_reduce, Cutout2D
+from matplotlib import pyplot as plt
+import matplotlib.colors as clr
+import os
+import re
+
+def save_image(image, name = 'Image', path = os.getcwd()):
+    """
+    image = image data
+    name = name of the image, default is Image
+    path = path of the resultant image,
+        default is the the present working directory
+    """
+    fig, ax = plt.subplots(1, 1, figsize = (5,10))
+    im = ax.imshow(image, origin = 'lower', norm = clr.LogNorm(vmin=3, vmax=20000),cmap='PuBu_r')
+    fig.colorbar(im, ax = ax, fraction = 0.046, pad = 0.04)
+    fig.savefig(path, name + '.png')
+
+
+
+def find_nearest_dark_exposure(image, dark_exposure_times, tolerance=0.5):
+    """
+    Find the nearest exposure time of a dark frame to the exposure time of the image,
+    raising an error if the difference in exposure time is more than tolerance.
+
+    This function is taken from the CCD Data reduction guide:
+    https://mwcraig.github.io/ccd-as-book/00-00-Preface.html
+    
+    Parameters
+    ----------
+    
+    image : astropy.nddata.CCDData
+        Image for which a matching dark is needed.
+    
+    dark_exposure_times : list
+        Exposure times for which there are darks.
+    
+    tolerance : float or ``None``, optional
+        Maximum difference, in seconds, between the image and the closest dark. Set
+        to ``None`` to skip the tolerance test.
+    
+    Returns
+    -------
+    
+    float
+        Closest dark exposure time to the image.
+    """
+
+    dark_exposures = np.array(list(dark_exposure_times))
+    idx = np.argmin(np.abs(dark_exposures - image.header['exptime']))
+    closest_dark_exposure = dark_exposures[idx]
+
+    if (tolerance is not None and 
+        np.abs(image.header['exptime'] - closest_dark_exposure) > tolerance):
+        return None
+        #raise RuntimeError('Closest dark exposure time is {} for flat of exposure '
+                           #'time {}.'.format(closest_dark_exposure, image.header['exptime']))
+        
+    
+    return closest_dark_exposure
+
+
+def inverse_median(a):
+    return 1/np.median(a)
+
+#------------------------------------------------------------------------------------------
+#-------------------------------Natural Sorting--------------------------------------------
+#------------------------------------------------------------------------------------------
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+#------------------------------------------------------------------
+#-----------------------Special Maxima-----------------------------
+#------------------------------------------------------------------
+"""
+This function finds a maximum among a dataset which
+is not a Dirac-delta.
+"""
+def special_maxi(x_array):
+    x_ary = x_array
+    yy = np.max(x_ary)
+    abc = np.where(x_ary == yy)
+    if abc[0][0] == 255:
+        cd = x_ary[abc[0][0]-1]
+    else:
+        cd = (x_ary[abc[0][0]-1]+x_ary[abc[0][0]+1])/2
+    if (yy-cd)>5:
+        x_ary[abc[0][0]] = 0
+        yy = np.max(x_ary)
+    return yy, x_ary
+
+def special_maximum(x_array):
+    x_ary = x_array
+    maxi = True
+    while maxi:
+        aaa, xnew = special_maxi(x_ary)
+        aaa1, xnew1 = special_maxi(xnew)
+        if aaa == aaa1:
+            maxi = False
+        else:
+            aaa = aaa1
+            xnew = xnew1
+    return aaa
+
+#------------------------------------------------------------------
+#--------------Polynomial of arbitraty degree----------------------
+#------------------------------------------------------------------
+
+def arbi_poly(x, *params):
+    """
+    Parameters
+    ----------
+    x : numpy.ndarray
+        takes the values of x where the value of polynomial is needed
+    *params: tuple, numpy.ndarray
+        values of coefficients of the polynomial
+        lowest degree first
+    ----------
+    returns
+    ----------
+    numpy.ndarray : values of polynomial at desired x-values
+    """
+    return sum([p*(x**i) for i, p in enumerate(params)])
