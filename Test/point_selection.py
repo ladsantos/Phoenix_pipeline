bfb0f3bca360dee29fa68457aced078a7bb112fa
Jayshil <jayshilpatel147@gmail.com>
2021-02-21 00:04:36 +0530

    Minor changes

--- Test/point_selection.py
+++ Test/point_selection.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from matplotlib.widgets import Cursor
 
 pt = os.getcwd()
+print(pt)
 
-pix, fl , fle = np.loadtxt(pt + '/cosmic_normal_sky_sub_2009oct30_0009.fits_flux.dat', usecols=(0,1,2), unpack=True)
+pix, fl , fle = np.loadtxt(pt + '/Test/cosmic_normal_sky_sub_2009oct30_0009.fits_flux.dat', usecols=(0,1,2), unpack=True)
+
 
 plt.errorbar(pix, fl, yerr=fle)
+
 
 data = plt.ginput(n=-1)
 print(data)
